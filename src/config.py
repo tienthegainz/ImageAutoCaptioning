4f34ff2bc5cde66d09fd6f072ce15c771edeaf65
sad1503 <vuductran153@gmail.com>
2019-06-13 23:38:04 +0700

    rename pickle file

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ word_threshold = 5
 embedding_dim = 200
 
 # Image feature Vector
-image_feature_file = 'image_feature/vector_features.pkl'
+image_feature_file = 'image_feature/image_vector.pkl'
 
 # Train configure
 batch_size = 10
